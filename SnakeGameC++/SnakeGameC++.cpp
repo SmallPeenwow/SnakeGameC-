df3660f2057f5a41d6746fb06199d4fd3370ee4b
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-19 13:38:22 +0200

    Snake Game Is Completed

--- SnakeGameC++/SnakeGameC++.cpp
+++ SnakeGameC++/SnakeGameC++.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
 bool gameOver;
-const int width = 20;
+const int width = 40;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
@@ -125,6 +126,8 @@ void Logic()
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
 
     for (int i = 1; i < nTail; i++)
     {
@@ -156,9 +159,35 @@ void Logic()
         break;
     }
 
-    if (x > width || x < 0 || y > height || y < 0) 
+    /*if (x > width || x < 0 || y > height || y < 0) 
     {
         gameOver = true;
+    }*/
+
+    if (x >= width)
+    {
+        x = 0;
+    }
+    else if (x < 0)
+    {
+        x = width - 1;
+    }
+    
+    if (y >= height)
+    {
+        y = 0;
+    }
+    else if (y < 0)
+    {
+        y = height - 1;
+    }
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+        {
+            gameOver = true;
+        }
     }
 
     if (x == fruitX && y == fruitY)
@@ -181,7 +210,7 @@ int main()
         Draw();
         Input();
         Logic();
-        //Sleep(10);
+        Sleep(10);
     }
 
     return 0;
